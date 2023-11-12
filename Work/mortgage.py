7a834e4951d2ae49f9d4d03b9dd6d27c3f52c026
especx34 <149973217+especx34@users.noreply.github.com>
2023-11-11 21:28:29 -0500

    Update mortgage.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -5,9 +5,17 @@ principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
+num_months = 0
+extra_pmt = 1000
+i = 0
 
 while principal > 0:
   principal = principal * (1+ rate/12) - payment
   total_paid = total_paid + payment
+  if i < 12:
+    principal = principal - extra_pmt
+    total_paid = total_paid + extra_pmt
+    i = i + 1
+  num_months = num_months +1
 
-print('Total paid',total_paid)
+print('Total paid',total_paid, num_months,'months')
