1a6f0a8e485e64d7ce1fd20cbfe10f911244d1e5
especx34 <149973217+especx34@users.noreply.github.com>
2024-01-18 18:11:40 -0500

    Update readfile.py

--- Work/readfile.py
+++ Work/readfile.py
@@ -2,3 +2,29 @@ with open('Data/portfolio.csv','rt') as f:
   data = f.read()
 data
 print(data)
+
+#line by line
+with open('Data/portfolio.csv','rt') as f:
+  for line in f:
+    print(line, end='')
+
+#skip a line
+f = open('Data/portfolio.csv','rt')
+headers = next(f)
+headers
+
+for line in f:
+  print(line, end='')
+
+f.close()
+
+#split a line
+f = open('data/portfolio.csv','rt')
+headers = next(f).split(',')
+headers
+
+for line in f:
+  row = line.split(',')
+  print(row)
+
+f.close()
