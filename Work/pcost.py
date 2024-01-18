c8997c22ec1b3b1bab47d21ff84d596395f0c6f6
especx34 <149973217+especx34@users.noreply.github.com>
2024-01-18 18:14:32 -0500

    Update pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,11 @@
 # pcost.py
 #
 # Exercise 1.27
+
+pcost = 0
+with open('Data/portfolio.csv','rt') as f:
+	for line in f:
+		row = line.split(',')
+		pcost += int(row[1]) * float(row[2])
+
+print(pcost)
